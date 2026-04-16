753fef20cd465cbe9360d065a9bbed59473beb55
robertoalex2323 <betodokij15@gmail.com>
2026-04-16 01:17:38 -0500

    actualización

---
+++ src/main/resources/static/Js/Login.js
@@ -0,0 +1,16 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const user = document.getElementById("user").value;
+  const pass = document.getElementById("pass").value;
+  const msg = document.getElementById("msg");
+
+  if (user === "admin" && pass === "1234") {
+    msg.style.color = "green";
+    msg.textContent = "Login correcto ✔";
+    alert("Bienvenido a VetCare");
+  } else {
+    msg.style.color = "red";
+    msg.textContent = "Usuario o contraseña incorrectos";
+  }
+});
